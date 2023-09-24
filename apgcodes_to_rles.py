ed9f86b966c2d49c0e628a3a39f96c26e0bdf31e
Carson Cheng <carsoncheng@MacBook-Air.local>
2023-09-24 08:43:40 +0800

    added some rles back

--- apgcodes_to_rles.py
+++ apgcodes_to_rles.py
@@ -3,7 +3,7 @@ import lifelib, glob
 sess = lifelib.load_rules('b3s23')
 lt = sess.lifetree(n_layers=1)
 files = glob.glob("apgcodes/*.txt")
-limits = [119, 1048576]
+limits = [0, 1048576]
 includes = [486, 972]
 for file in files:
     rles = ""
@@ -12,8 +12,8 @@ for file in files:
         prefix = file.split("/")[-1].split(".")[0]
         period = int(prefix.split("xp")[1])
         # if-statement can be removed if you need to update all periods
-        #if period < limits[1] and period > limits[0]:
-        if period in includes:
+        if period < limits[1] and period > limits[0]:
+            #if period in includes:
             apgcodes = f.read().split("\n")
             for apgcode in apgcodes:
                 if "x" in apgcode:
